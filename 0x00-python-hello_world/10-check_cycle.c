86e7dedc1c048ebdb317badb933a04fa6fddea57
Tassuo7 <abdelhamid.jack@gmail.com>
2023-05-08 18:42:10 -0700

    finishing mandatory tasks

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * check_cycle - function that checks if a LL has a cycle
+ * @list: the linked list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *lista;
+
+	if ((list) || (list->next))
+		return (0);
+	lista = list->next;
+	while (lista != NULL && lista->next != NULL)
+	{
+		if (lista == list)
+			return (1);
+		list = lista;
+		lista = lista->next;
+	}
+	return (0)
+}
