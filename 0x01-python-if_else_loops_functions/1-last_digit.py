6f5087e1d376d4eec54fc2c7d030ab2690249b53
Tassuo7 <abdelhamid.jack@gmail.com>
2023-05-09 07:01:02 -0700

    fix negative case

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 last = int(repr(number)[-1])
+if number < 0:
+    last = - last
 if last > 5:
     print(f"Last digit of {number} is {last} and is greater than 5")
 elif last == 0:
