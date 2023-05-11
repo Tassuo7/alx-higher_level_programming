30f273f4916b9f942bc5d602cf912fd1c0519af7
Tassuo7 <abdelhamid.jack@gmail.com>
2023-05-11 13:43:19 -0700

    importing sys and fix condition

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
         print("1 argument:\n1: {}".format(argv[1]))
     else:
         print("{} arguments:".format(len(argv) - 1))
-        for i in range(len(argv)):
+        for i in range(len(argv) - 1):
             print("{}: {}".format(i + 1, argv[i + 1]))
