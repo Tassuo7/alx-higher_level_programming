85bece740f35c60ce1956de8bff6bf7f46d7a7f0
Tassuo7 <abdelhamid.jack@gmail.com>
2023-05-11 15:40:04 -0700

    second calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import (add, sub, mul, div)
+    from sys import argv
+
+    if (len(argv) - 1) != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    if operator == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif operator == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif operator == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif operator == '/':
+        print("{} * {} = {}".format(a, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
