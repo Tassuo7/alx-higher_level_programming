6e564b8df996603031ccb064cbedd1ab79e7640b
Tassuo7 <abdelhamid.jack@gmail.com>
2023-05-11 16:10:01 -0700

    testing with calculator1

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     if (len(argv) - 1) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    a = argv[1]
-    b = argv[3]
+    a = int(argv[1])
+    b = int(argv[3])
     operator = argv[2]
     if operator == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
