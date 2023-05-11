bbc91b5ffb49b6b56c01f9abe2d5580e927a7bf9
Tassuo7 <abdelhamid.jack@gmail.com>
2023-05-11 13:41:01 -0700

    importing sys and fix condition

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,11 +2,11 @@
 if __name__ == "__main__":
     from sys import argv
 
-    if len(argv) == 0:
+    if (len(argv) - 1) == 0:
         print("0 arguments.")
-    elif len(argv) == 1:
-        print("1 argument:\n1: {}".format(argv))
+    elif (len(argv) - 1) == 1:
+        print("1 argument:\n1: {}".format(argv[1]))
     else:
-        print("{} arguments:".format(len(argv)))
+        print("{} arguments:".format(len(argv) - 1))
         for i in range(len(argv)):
             print("{}: {}".format(i + 1, argv[i + 1]))
