9cf8306a79e8b64f402672588401da77fd062423
Tassuo7 <abdelhamid.jack@gmail.com>
2023-05-17 17:43:36 -0700

    change from items to range

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
         return 0
     rmn = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     num = 0
-    for i in roman_string:
+    for i in range(len(roman_string)):
         if rmn[roman_string[i]] < rmn[roman_string[i + 1]]:
             num -= rmn[roman_string[i]]
         else:
