0bd32eb26a23e86deadb40f31082f2378d5d6987
Tassuo7 <abdelhamid.jack@gmail.com>
2023-05-17 18:04:38 -0700

    assign i before use

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,6 +4,7 @@ def roman_to_int(roman_string):
         return 0
     rmn = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     num = 0
+    i = 0
     for i in range(len(roman_string) - 1):
         if rmn[roman_string[i]] < rmn[roman_string[i + 1]]:
             num -= rmn[roman_string[i]]
