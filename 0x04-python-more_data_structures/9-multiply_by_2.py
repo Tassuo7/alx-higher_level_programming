7de0d6b97a138f4b538487b369126b0bbf515fc2
Tassuo7 <abdelhamid.jack@gmail.com>
2023-05-17 17:05:16 -0700

    forget them

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_d = {}
+    new_dic = {}
     for key, val in a_dictionary.items():
-        new_d
+        new_dic.update({key: (val * 2)})
+    return new_dic
