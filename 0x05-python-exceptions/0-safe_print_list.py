60dec3e82145f2706f3b8e73466f817cecf36310
Tassuo7 <abdelhamid.jack@gmail.com>
2023-05-29 18:53:07 -0700

    fix errors

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     hmx = 0
-    try:
-        for elem in range(x):
+    for elem in range(x):
+        try:
             print("{:d}".format(my_list[elem]), end="")
             hmx += 1
-    except IndexError:
-        break
-    print()
+        except IndexError:
+            break
+    print("")
     return hmx
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -14,5 +14,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             divs = 0
         finally:
-            list_n.append(divs)
-    return list_n
+            newlist.append(divs)
+    return newlist
