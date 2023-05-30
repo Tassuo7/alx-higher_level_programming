bb030753d1a6da4b354f012e3e2783354e76887e
Tassuo7 <abdelhamid.jack@gmail.com>
2023-05-29 18:56:30 -0700

    fix errors

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     hmx = 0
     for elem in range(0, x):
         try:
-            print("{:d}".format(my_list[index]), end="")
+            print("{:d}".format(my_list[elem]), end="")
             hmx += 1
         except (ValueError, TypeError):
             continue
