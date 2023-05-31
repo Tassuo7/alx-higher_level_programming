876617fa6df74327a7c10825d1589b8af7ec0377
Tassuo7 <abdelhamid.jack@gmail.com>
2023-05-30 19:51:43 -0700

    initialize position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -7,8 +7,9 @@ class Square:
     """define a square with Private instance attribute: size
     Instantiation with optional size"""
 
-    def __init__(self, size=0):
+    def __init__(self, size=0 position=(0, 0)):
         self.__size = size
+        self.__position = position
 
     @property
     def size(self):
