10e21410d0fc8af91d46a6532037662fe7c4b5bf
Tassuo7 <abdelhamid.jack@gmail.com>
2023-05-31 18:08:31 -0700

    fix it and test

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -57,4 +57,4 @@ class Square:
             print()
 
         for i in range(self.__size):
-            print(" " * self._position[0] + "#" * self.__size)
+            print(" " * self.__position[0] + "#" * self.__size)
