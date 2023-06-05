5e8e5c2040dcfcfc3d5226e1130adcf6bf2bc0d9
Tassuo7 <abdelhamid.jack@gmail.com>
2023-06-05 11:02:57 -0700

    N queens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -3,13 +3,13 @@
 
 
 import sys
-if len(sys.args) != 2:
+N = sys.argv[1]
+if len(sys.argv) != 2:
     print("Usage: nqueens N")
     exit(1)
-N = sys.args[1]
-if type(N) is not int:
+if N.isdigit() is False:
     print("N must be a number")
     exit(1)
-if N < 4:
+if int(N) < 4:
     print("N must be at least 4")
     exit(1)
