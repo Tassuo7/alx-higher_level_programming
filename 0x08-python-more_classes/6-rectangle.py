7ccbd55f63a20ab054bbd8a23a90d958779dfde5
Tassuo7 <abdelhamid.jack@gmail.com>
2023-06-05 10:39:21 -0700

    num of inst

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -10,10 +10,13 @@ class Rectangle:
     and print msg when deleted
     and calculate number of instances"""
 
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """initialisation of instances width and height"""
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
