4c7426c15b9c3331af091ce7d75803f7b630ede7
Tassuo7 <abdelhamid.jack@gmail.com>
2023-06-13 19:26:07 -0700

    10 students

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" a class Student that defines a student"""
+
+
+class Student:
+    """defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation of a Student instance"""
+       if (type(attrs) == list and all([type(nr) == str for nr in attrs])):
+           return {k: getattr(self, k) for k in attrs if k in attrs}
+       return self.__dict__
