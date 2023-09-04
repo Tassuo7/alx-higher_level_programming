80c968bfc053045206e59370c1f8adde33164f0c
Tassuo7 <abdelhamid.jack@gmail.com>
2023-09-04 14:47:51 -0700

    fix 8 error

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -11,9 +11,9 @@ if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
     q = "" if len(sys.argv) == 1 else sys.argv[1]
     try:
-        data = req.post(u, data={'q': q}).json()
-        if data:
-            print(f"[{data['id']}] {data['name']}")
+        dt = req.post(url, data={'q': q}).json()
+        if dt:
+            print(f"[{dt['id']}] {dt['name']}")
         else:
             print("No result")
     except ValueError:
