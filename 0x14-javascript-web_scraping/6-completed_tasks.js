e8fea2ca4edd6ddde6c50b148b1cf354faab565c
Tassuo7 <abdelhamid.jack@gmail.com>
2023-09-26 17:19:42 -0700

    semistandard fix

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,7 +2,7 @@
 const request = require('request');
 const url = process.argv[2];
 request.get(url, function (err, response, body) {
-  if (err) console.log(error);
+  if (err) console.log(err);
   else if (response.statusCode === 200) {
     const done = {};
     for (let i = 0; i < JSON.parse(body).length; i++) {
