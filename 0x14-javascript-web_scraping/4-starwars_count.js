786a7b3658343fc8a73149cdb1e6148f2b136045
Tassuo7 <abdelhamid.jack@gmail.com>
2023-09-26 17:01:42 -0700

    fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,13 +1,24 @@
 #!/usr/bin/node
+
 const request = require('request');
 const url = process.argv[2];
+
 request(url, function (err, response, body) {
-  if (!err) {
-    const data = JSON.parse(body).results;
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
     let count = 0;
-    data.forEach(film => {
-      if (film.character.endsWith('/18/')) count++;
-    });
+    for (const f in films) {
+      const caract = films[f].characters;
+      for (const c in caract) {
+        if (caract[c].includes('18')) {
+          count++;
+        }
+      }
+    }
     console.log(count);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
 });
